4d06f02a74027ffcacf89b4239e6ab83194b1c7b
Paul Rail <paul.a.rail@gmail.com>
2019-10-24 23:32:07 -0400

    add method to calculate_total_cost for a customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -8,6 +8,12 @@ module Api::V1
         # puts customers
         render json: {status:'SUCCESS', message:'Loaded customers', data:customers},status: :ok
       end
+
+      # def calculate_cost
+      #   total_cost = 0
+      #   flat_fee_per_item = 
+      #   total_cost = 
+      # end
     end
 #   end
 # end
